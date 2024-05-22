ab944c91d30bd5c164401ec2f559b5afee7c44da
Yunthecloud <sulei.yin1@kmutt.ac.th>
2024-05-23 00:09:01 +0700

    [add] debug show checked false

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'retro_vault',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
